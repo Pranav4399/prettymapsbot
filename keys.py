f3a9552d307744bfde01af1e58eee38be9aa4c84
Pranav Sriram <psriram37@gmail.com>
2022-04-11 18:54:45 +0530

    Pilot commit

---
+++ keys.py
@@ -0,0 +1,6 @@
+# Twitter application consumer keys and access tokens.
+# Remember, keep this information private.
+consumer_key = 'bnxGBBOoTXI2neH4DUMHNyJkI'
+consumer_secret = 'fnHFT87Eri76YCfh2QNKLz2ibxyK3NsN9iL9swotqlFIgYTGs8'
+access_token = '1512335497464483844-jc9ubFpTonEbkarcKRK1bIOdArvU2f'
+access_token_secret = 'qgzyUcGi4ZHtvHjTIYTsKRz1DbWK8g2TlVPtT3BSXPGKx'
